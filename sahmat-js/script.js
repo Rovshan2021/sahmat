f3c403bd7179a78ef89bb0cdfb2a89dfb1e36f8e
Rovshan2021 <87198333+Rovshan2021@users.noreply.github.com>
2021-08-23 19:09:36 +0400

    Add files via upload

---
+++ sahmat-js/script.js
@@ -0,0 +1,50 @@
+function cedvel(){
+let cedvel = document.getElementById("cedvel");
+let cvb ="";
+for(let i = 1; i<=8; i++){
+    cvb += `<tr>`;
+    for(let i = 1; i<=8; i++){
+        cvb += `<td> </td>`;
+    }
+    cvb += `</tr>`;
+    
+}
+ 
+
+
+cedvel.innerHTML=cvb;
+
+}
+cedvel();
+let td = document.getElementsByTagName("td")[0].style.backgroundColor = "black"
+document.getElementsByTagName("td")[2].style.backgroundColor = "black"
+document.getElementsByTagName("td")[4].style.backgroundColor = "black"
+document.getElementsByTagName("td")[6].style.backgroundColor = "black"
+document.getElementsByTagName("td")[9].style.backgroundColor = "black"
+document.getElementsByTagName("td")[11].style.backgroundColor = "black"
+document.getElementsByTagName("td")[13].style.backgroundColor = "black"
+document.getElementsByTagName("td")[15].style.backgroundColor = "black"
+document.getElementsByTagName("td")[16].style.backgroundColor = "black"
+document.getElementsByTagName("td")[18].style.backgroundColor = "black"
+document.getElementsByTagName("td")[20].style.backgroundColor = "black"
+document.getElementsByTagName("td")[22].style.backgroundColor = "black"
+document.getElementsByTagName("td")[25].style.backgroundColor = "black"
+document.getElementsByTagName("td")[27].style.backgroundColor = "black"
+document.getElementsByTagName("td")[29].style.backgroundColor = "black"
+document.getElementsByTagName("td")[31].style.backgroundColor = "black"
+document.getElementsByTagName("td")[32].style.backgroundColor = "black"
+document.getElementsByTagName("td")[34].style.backgroundColor = "black"
+document.getElementsByTagName("td")[36].style.backgroundColor = "black"
+document.getElementsByTagName("td")[38].style.backgroundColor = "black"
+document.getElementsByTagName("td")[41].style.backgroundColor = "black"
+document.getElementsByTagName("td")[43].style.backgroundColor = "black"
+document.getElementsByTagName("td")[45].style.backgroundColor = "black"
+document.getElementsByTagName("td")[47].style.backgroundColor = "black"
+document.getElementsByTagName("td")[48].style.backgroundColor = "black"
+document.getElementsByTagName("td")[50].style.backgroundColor = "black"
+document.getElementsByTagName("td")[52].style.backgroundColor = "black"
+document.getElementsByTagName("td")[54].style.backgroundColor = "black"
+document.getElementsByTagName("td")[57].style.backgroundColor = "black"
+document.getElementsByTagName("td")[59].style.backgroundColor = "black"
+document.getElementsByTagName("td")[61].style.backgroundColor = "black"
+document.getElementsByTagName("td")[63].style.backgroundColor = "black";
